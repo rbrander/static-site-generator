4431f445dde1cdb770ec1013d12bcec14049b709
Rob Brander <rob.brander@gmail.com>
2026-01-18 12:32:01 -0500

    create main and text node

---
+++ src/textnode.py
@@ -0,0 +1,25 @@
+from enum import Enum
+
+class TextType(Enum):
+  PLAIN_TEXT = "plain text"
+  BOLD_TEXT = "bold" # **bold**
+  ITALIC_TEXT = "italic" # _italic_
+  CODE_TEXT = "code" # `code`
+  LINK = "link" # [anchor text](url)
+  IMAGE = "image" # ![alt text](url)
+
+class TextNode:
+  def __init__(self, text, text_type: TextType, url = None):
+    self.text = text
+    self.text_type = text_type
+    self.url = url
+
+  def __eq__(self, other):
+    return (
+      self.text == other.text and
+      self.text_type == other.text_type and
+      self.url == other.url
+    )
+
+  def __repr__(self):
+    return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
