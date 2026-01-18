809b819f52aae291355e246953e22f083d2c51e6
Rob Brander <rob.brander@gmail.com>
2026-01-18 13:52:23 -0500

    add ParentNode

--- src/main.py
+++ src/main.py
@@ -1,11 +1,23 @@
 # from textnode import TextNode, TextType
 # from htmlnode import HTMLNode
 from leafnode import LeafNode
+from parentnode import ParentNode
 
 def main():
   #node = TextNode("This is some anchor text",TextType.LINK ,"https://www.boot.dev")
   #node = HTMLNode(tag="p", value="Hello World!", props={"color":"red"})
-  node = LeafNode(tag="p", value="Hello World!", props={"color":"red"})
+  #node = LeafNode(tag="p", value="Hello World!", props={"color":"red"})
+
+  node = ParentNode(
+    "p",
+    [
+        LeafNode("b", "Bold text"),
+        LeafNode(None, "Normal text"),
+        LeafNode("i", "italic text"),
+        LeafNode(None, "Normal text"),
+    ],
+  )
+
   print(node)
   print(node.to_html())
 
---
+++ src/test_parentnode.py
@@ -0,0 +1,30 @@
+import unittest
+from leafnode import LeafNode
+from parentnode import ParentNode
+
+class TestParentNode(unittest.TestCase):
+  def test_missing_children(self):
+    node = ParentNode(tag="p", children=None)
+    self.assertRaises(ValueError, node.to_html)
+
+  def test_missing_tag(self):
+    node = ParentNode(tag=None, children=[LeafNode("span", "child")])
+    self.assertRaises(ValueError, node.to_html)
+
+  def test_to_html_with_no_children(self):
+    parent_node = ParentNode(tag="div", children=[])
+    self.assertEqual(parent_node.to_html(), "<div></div>")
+
+  def test_to_html_with_children(self):
+    child_node = LeafNode("span", "child")
+    parent_node = ParentNode("div", [child_node])
+    self.assertEqual(parent_node.to_html(), "<div><span>child</span></div>")
+
+  def test_to_html_with_grandchildren(self):
+    grandchild_node = LeafNode("b", "grandchild")
+    child_node = ParentNode("span", [grandchild_node])
+    parent_node = ParentNode("div", [child_node])
+    self.assertEqual(
+        parent_node.to_html(),
+        "<div><span><b>grandchild</b></span></div>",
+    )
