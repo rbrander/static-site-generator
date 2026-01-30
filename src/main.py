86fd7b45ae0e7c10196c7c74afd36db11e86118b
Rob Brander <rob.brander@gmail.com>
2026-01-30 14:08:01 -0500

    create static folder and add copy directory function

--- src/main.py
+++ src/main.py
@@ -1,9 +1,14 @@
+import shutil
 from textnode import TextNode, TextType
 
 
+def copyDirectory(source:str, destination:str):
+    print(f"Copying content from {source} to {destination}")
+    shutil.rmtree(destination)
+    shutil.copytree(source, destination)
+
 def main():
-    node = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
-    print(node)
+    copyDirectory("./static", "./public")
 
 
 main()
